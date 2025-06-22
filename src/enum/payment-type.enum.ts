63333884d9305fcf61fbdcfb85e55a68292d17b6
isnael07 <isnaellima24@gmail.com>
2025-06-21 22:10:43 -0300

    feat[]:adiciona module de payment

---
+++ src/enum/payment-type.enum.ts
@@ -0,0 +1,5 @@
+export enum PaymentType {
+  CARD = 'CARD',
+  CASH = 'CASH',
+  PIX = 'PIX',
+}
---
+++ src/payment/entities/payment.entity.ts
@@ -0,0 +1,64 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  ManyToOne,
+  JoinColumn,
+  BeforeUpdate,
+  BeforeInsert,
+ 
+} from 'typeorm';
+import { Order } from 'src/order/entities/order.entity';
+import { Customer } from 'src/customer/entities/customer.entity';
+import { PaymentType } from 'src/enum/payment-type.enum';
+import { OrderStatus } from 'src/enum/order-status.enum';
+
+@Entity('payments')
+export class Payment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => Order, (order) => order.payment, {
+    cascade: true,
+    eager: true,
+  })
+  @JoinColumn()
+  order: Order;
+
+  @Column('decimal', { precision: 10, scale: 2, nullable: false })
+  amount: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @ManyToOne(() => Customer, { cascade: true, eager: true })
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @Column({ type: 'enum', enum: PaymentType })
+  paymentType: PaymentType;
+
+  @Column({ type: 'enum', enum: OrderStatus })
+  status: OrderStatus;
+
+  constructor() {
+    this.status = OrderStatus.INITIATED;
+    this.date = new Date();
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  calculateFinalAmount(): void {
+    if (this.order?.orderItems?.length) {
+      const total = this.order.orderItems
+        .map((item) => item.product.price)
+        .reduce((sum, price) => sum + Number(price), 0);
+      this.amount = Number(total.toFixed(2));
+    } else {
+      this.amount = 0;
+    }
+  }
+
+}
+
