b3904ab6753553d26d8e80e7448e082203c659c6
ismaellima4 <ismael.github@gmail.com>
2025-06-27 14:49:16 -0300

    chore[]: adiciona validador de cnpj

--- src/supplier/dto/create-supplier.dto.ts
+++ src/supplier/dto/create-supplier.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
 
 export class CreateSupplierDto {
   @IsString()
@@ -7,6 +14,14 @@ export class CreateSupplierDto {
   @IsString()
   @IsNotEmpty()
   @IsOptional()
+  @Length(14, 18, {
+    message:
+      'O CNPJ deve ter entre 14 e 18 caracteres (com ou sem formatação).',
+  })
+  @Matches(/^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$/, {
+    message:
+      'CNPJ inválido. Use o formato XX.XXX.XXX/XXXX-XX ou apenas números.',
+  })
   cnpj?: string;
   @IsString()
   @IsNotEmpty()
